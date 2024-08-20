678f29603ec5165715eced324ec7a5fbbd6c3be6
Sritha Sriram <srithasriram30@gmail.com>
2024-08-20 10:40:27 +0800

    Add responsive nabvar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import './navbar.scss'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
         <div className="left">
@@ -16,7 +18,17 @@ const Navbar = () => {
         <div className="right">
             <a href="/">Sign In</a>
             <a href="/" className='register'>Sign Up</a>
-
+            <div className="menuIcon" onClick={() => setOpen((prev) => !prev)}>
+              <img src="/menu.png" alt="hamburger menu" />
+            </div>
+            <div className="menu" className={open? "menu active" : "menu"}>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Agents</a>
+            <a href="/">Sign In</a>
+            <a href="/">Sign Up</a>
+            </div>
         </div>
     </nav>
   )
