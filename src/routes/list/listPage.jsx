f82910fac42e7f2dc22ee3f28abd4edaa6cbc200
Sritha Sriram <srithasriram30@gmail.com>
2024-09-26 16:58:13 +0800

    Complete map

--- src/routes/list/listPage.jsx
+++ src/routes/list/listPage.jsx
@@ -1,9 +1,26 @@
 import './listPage.scss';
-
+import Filter from '../../components/filter/Filter'
+import { listData } from '../../lib/dummyData'
+import Card from '../../components/card/Card';
+import Map from '../../components/map/Map';
 const ListPage = () => {
+
+  const data = listData;
   return (
     <div className='listPage'>
-        List
+        <div className="listContainer">
+          <div className="wrapper">
+            <Filter />
+            {
+              data.map(item => (
+                <Card key={item.id} item={item}/>
+              ))
+            }
+          </div>
+        </div>
+        <div className="mapContainer">
+          <Map items={data}/>
+        </div>
     </div>
   )
 }
